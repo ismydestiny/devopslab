6e82a9be1e7ce8f6c926f901f466a277a34144c2
ismydestiny <49895414+ismydestiny@users.noreply.github.com>
2019-12-07 14:36:51 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=ismydestiny
 
 export MYCLUSTER=tektonknativebeijing11
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing11/kube-config-tok02-tektonknativebeijing11.yml
 
 export NAMESPACE=shawn
 export EMAIL=hanxiaowenhao@gmail.com
